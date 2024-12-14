cc6db462e7f4b9b607a01a1dcf8990056e3880de
osxar <osxar132435@gmail.com>
2024-12-14 15:49:54 -0500

    version1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className='container full column'>
+      <h1 className="winter">Los toxicos</h1>
+      <a href="minecraft://?addExternalServer=Los Toxicos :v|20.220.173.216:19132">
+        <button>Unete :v</button>
+      </a>
+    </div>
+  )
+}
+
+export default App
