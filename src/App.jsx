042d7eafa336f009ddcd28c81e1f9381ff46b6d2
OSX <osxar132435@gmail.com>
2025-04-12 06:46:02 -0500

    Cambio de redireccionamiento de server

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className='container full column'>
       <h1 className="winter">Los toxicos</h1>
-      <a href="minecraft://?addExternalServer=Los Toxicos :v|20.220.173.216:19132">
+      <a href="minecraft://?addExternalServer=Ponganle nombre a esta madre 🗣️|15.235.118.68:2675">
         <button>Unete :v</button>
       </a>
     </div>
