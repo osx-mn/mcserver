a9142d87d41114631688b075f10f0ddf440b145e
OSX <osxar132435@gmail.com>
2025-04-26 08:28:34 -0500

    añadiendo 3 nuevas imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,12 @@ import Image4 from './assets/image4.png'
 import Image5 from './assets/image5.png'
 import Image6 from './assets/image6.png'
 import Image7 from './assets/image7.png'
+import Image8 from './assets/image8.png'
+import Image9 from './assets/image9.png'
+import Image10 from './assets/image10.png'
 
 const Miembros= ["Kiritokun009", "MadaB20", "Mxrsyi", "srOxmans"]
-const images= [Image1, Image2, Image3, Image4, Image5, Image6, Image7]
+const images= [Image10, Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8, Image9]
 
 const animationDuration= 10000;
 
